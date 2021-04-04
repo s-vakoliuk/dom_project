030c11c84c7569b2e48145a56070db50e6b3ed7e
SerhiiVakoliuk <s.vakoliuk@gmail.com>
2021-04-04 23:13:05 +0300

    April 04 2021

--- script02.js
+++ script02.js
@@ -1,69 +1,79 @@
-//За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Вставити цей блок на сторінку
-//let div = document.createElement('div');
-//div.innerHTML = "<strong>Всім привіт!</strong> Ви прочитали важливе повідомлення.";
-//document.body.append(div);
-
 let users = [{
   name: 'vasya',
   age: 31,
   status: false,
-  address: { city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1 }
+  address: { country: 'Ukraine', city: 'Lviv', street: 'Shevchenko', houseNumber: 1 }
 }, {
   name: 'petya',
   age: 30,
   status: true,
-  address: { city: 'New York', country: 'USA', street: 'East str', houseNumber: 21 }
+  address: { country: 'USA', city: 'New York', street: 'East str', houseNumber: 21 }
 }, {
   name: 'kolya',
   age: 29,
   status: true,
-  address: { city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78 }
+  address: {country: 'Hungary',  city: 'Budapest', street: 'Kuraku', houseNumber: 78 }
 }, {
   name: 'olya',
   age: 28,
   status: false,
-  address: { city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56 }
+  address: {country: 'Czech', city: 'Prague', street: 'Paster', houseNumber: 56 }
 }, {
   name: 'max',
   age: 30,
   status: true,
-  address: { city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39 }
+  address: {country: 'Turkey', city: 'Istanbul', street: 'Mikar', houseNumber: 39 }
 }, {
   name: 'anya',
   age: 31,
   status: false,
-  address: { city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5 }
+  address: { country: 'Brasil', city: 'Rio', street: 'Ronaldi', houseNumber: 5 }
 }, {
   name: 'oleg',
   age: 28,
   status: false,
-  address: { city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90 }
+  address: { country: 'Canada', city: 'Montreal', street: 'Acusto', houseNumber: 90 }
 }, {
   name: 'andrey',
   age: 29,
   status: true,
-  address: { city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33 }
+  address: { country: 'Canada', city: 'Quebeck',  street: 'Binaro', houseNumber: 33 }
 }, {
   name: 'masha',
   age: 30,
   status: true,
-  address: { city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1 }
+  address: { country: 'Russia', city: 'Moscow', street: 'Gogolia', houseNumber: 1 }
 }, {
   name: 'olya',
   age: 31,
   status: false,
-  address: { city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4 }
+  address: { country: 'USA', city: 'Portland', street: 'Forest str', houseNumber: 4 }
 }, {
   name: 'max',
   age: 31,
   status: true,
-  address: { city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45 }
+  address: { country: 'Egypt', city: 'Cairo', street: 'Seashore', houseNumber: 45 }
 }];
+
+//За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Вставити цей блок на сторінку
 for (const user of users) {
-    const userdiv = document.createElement('div');
-    userdiv.innerHTML = `Ім'я: ${user.name} <br> Вік: ${user.age} <br> Адреса:${user.address} <br>`;
-    userdiv.style.border="1 px blue";
-    document.body.append(userdiv);
+    const userDiv = document.createElement('div');
+    userDiv.innerHTML = `Ім'я: ${user.name} <br> Вік: ${user.age} <br> Статус:${user.status}`;
+    userDiv.style.margin="10px";
+    userDiv.style.border="3px solid blue";
+    document.body.append(userDiv);
 }
 
 // За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Блок з адресою зробити окремим блоком, з блоками для кожної властивості
+for (const user of users) {
+    const userDiv = document.createElement('div');
+    userDiv.innerHTML = `Ім'я: ${user.name} <br> Вік: ${user.age} <br> Статус:${user.status}`;
+    userDiv.style.margin = "10px";
+    userDiv.style.border = "3px solid blue";
+    document.body.append(userDiv);
+    let adressDiv = document.createElement('div');
+    adressDiv.style.margin = "10px";
+    adressDiv.style.border = "3px solid yellow";
+    adressDiv.innerHTML = `Країна: ${user.address.country} <br> Місто: ${user.address.city} <br> Вулиця:${user.address.street}<br>Номер будинку:${user.address.houseNumber}`;
+    document.body.append(adressDiv);
+}
